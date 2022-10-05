154b49fe69d0dcec0b940a395f5168babc6a4aef
Andrew McInerney <andrew.mcinerney@ul.ie>
2022-10-05 16:23:27 +0100

    add selectnn functionality (temp)

---
+++ R/node_selection.R
@@ -0,0 +1,272 @@
+#' Selects hidden layer structure
+#'
+#' Performs either bulk or step-wise hidden node selection.
+#'
+#' @param X Matrix of covariates
+#' @param y Vector of response
+#' @param Q Candidate number of hidden nodes
+#' @param n_init Number of random initialisations (tracks)
+#' @param type Selection type: `"bulk"` (the default) or `"step"`
+#' @param inf_crit Information criterion: `"BIC"` (default), `"AIC"` or
+#'  `"AICc"`
+#' @param task `"regression"` (default) or `"classification"`
+#' @param unif Random initial values max value
+#' @param maxit maximum number of iterations for nnet (default = 100)
+#' @param ... additional argument for nnet
+#' @return Optimal number of hidden nodes
+#' @export
+hidden_node_sel <- function(X, y, Q, n_init, type = "bulk", inf_crit = "BIC",
+                            task = "regression", unif = 3, maxit = 1000, ...) {
+  if (type == "bulk") {
+    inf_crit_vec <- rep(NA, Q)
+
+    names(inf_crit_vec) <- as.character(1:Q)
+
+    weights_min <- vector("list", length = Q)
+
+    for (q in 1:Q) {
+      nn <- nn_fit_tracks(X, y, q, n_init, inf_crit, task, unif,
+                          maxit = maxit, ...
+      )
+
+      weights_min[[q]] <- nn$W_opt
+
+      inf_crit_vec[q] <- nn$value
+    }
+
+    W_opt <- weights_min[[which.min(inf_crit_vec)]]
+  } else if (type == "step") {
+    n_candidates <- 3
+
+    inf_crit_vec <- rep(NA, n_candidates)
+
+    names(inf_crit_vec) <- as.character((Q - 1):(Q + 1))
+
+    n <- nrow(X) # sample size
+    p <- ncol(X) # number of covariates
+
+    weights_min <- vector(mode = "list", length = n_candidates)
+    names(weights_min) <- as.character((Q - 1):(Q + 1))
+
+
+    for (q in (Q - 1):(Q + 1)) {
+      if (q == 0) {
+        weights_min[[as.character(q)]] <- NULL
+
+        inf_crit_vec[as.character(q)] <- NA
+      } else {
+        k <- (p + 1) * q + (q + 1)
+
+
+        nn <- nn_fit_tracks(X, y, q, n_init, inf_crit, task, unif,
+                            maxit = maxit, ...
+        )
+
+        weights_min[[as.character(q)]] <- nn$W_opt
+
+        inf_crit_vec[as.character(q)] <- nn$value
+      }
+    }
+
+    W_opt <- weights_min[[names(which.min(inf_crit_vec))]]
+  }
+
+  return(list(
+    "min" = as.numeric(names(which.min(inf_crit_vec))),
+    "value" = min(inf_crit_vec, na.rm = TRUE),
+    "inf_crit_vec" = inf_crit_vec,
+    "weights_min" = weights_min,
+    "W_opt" = W_opt
+  ))
+}
+
+
+#' Selects input layer structure
+#'
+#' Performs either bulk or step-wise input node selection.
+#'
+#' @param X Matrix of covariates
+#' @param y Vector of response
+#' @param q Number of hidden nodes
+#' @param n_init Number of random initialisations (tracks)
+#' @param type Selection type: `"bulk"` (the default) or `"step"`
+#' @param inf_crit Information criterion: `"BIC"` (default), `"AIC"` or
+#'  `"AICc"`
+#' @param task `"regression"` (default) or `"classification"`
+#' @param unif Random initial values max value
+#' @param X_full Full matrix of covariates if X has some dropped
+#' @param maxit maximum number of iterations for nnet (default = 100)
+#' @param ... additional argument for nnet
+#' @return Inputs dropped from model
+#' @export
+input_node_sel <- function(X, y, q, n_init, type = "bulk", inf_crit = "BIC",
+                           task = "regression", unif = 3, X_full = NULL,
+                           maxit = 1000, ...) {
+  init_model <- nn_fit_tracks(X, y, q, n_init, inf_crit, task, unif,
+                              maxit = maxit, ...
+  )
+
+  init_inf_crit <- init_model$value
+
+  p <- ncol(X)
+
+  X_init <- X
+
+  delta_bic <- NULL
+
+  dropped <- c()
+
+  added <- c()
+
+  W_opt <- init_model$W_opt
+
+  min_inf_crit <- init_inf_crit
+
+  if (type == "bulk") {
+    continue_drop <- TRUE
+
+    while (continue_drop == TRUE) {
+      nn_in <- input_importance(
+        X = X, y = y, q = q, n_init = n_init,
+        inf_crit = inf_crit, task = task, unif = unif, maxit = maxit,
+        ...
+      )
+
+      if (nn_in$value < min_inf_crit) {
+        X <- X[, -nn_in$min, drop = FALSE]
+        p <- ncol(as.matrix(X))
+
+
+        W_opt <- nn_in$W_opt
+
+        dropped <- colnames(X_init)[!colnames(X_init) %in% colnames(X)]
+        min_inf_crit <- nn_in$value
+
+        if (ncol(X) == 1) {
+          continue_drop <- FALSE
+        }
+      } else {
+        continue_drop <- FALSE
+      }
+    }
+  } else if (type == "step") {
+    if (is.null(X_full)) {
+      stop("Error: X_full must not be null when performing stepwise")
+    }
+
+
+    nn_in <- input_importance(
+      X = X, y = y, q = q, n_init = n_init,
+      inf_crit = inf_crit, task = task, unif = unif,
+      addition = TRUE, X_full = X_full, maxit = maxit,
+      ...
+    )
+
+    if (nn_in$value < min_inf_crit) {
+      if (names(nn_in$min) %in% colnames(X)) {
+        X <- X[, -nn_in$min, drop = FALSE]
+      } else {
+        X <- cbind(X, X_full[, names(nn_in$min), drop = FALSE])
+      }
+
+
+      p <- ncol(as.matrix(X))
+
+
+      W_opt <- nn_in$W_opt
+
+      dropped <- colnames(X_init)[!colnames(X_init) %in% colnames(X)]
+      added <- colnames(X)[!colnames(X) %in% colnames(X_init)]
+      min_inf_crit <- nn_in$value
+    }
+
+    delta_bic <- nn_in$inf_crit_vec - init_inf_crit
+  }
+  return(list(
+    "X" = X, "p" = p, "W_opt" = W_opt, "dropped" = dropped, "added" = added,
+    "init_value" = init_inf_crit, "value" = min_inf_crit, "delta_bic" = delta_bic
+  ))
+}
+
+
+#' Determines the importance of each input
+#'
+#' Calculates the importance of each input model based on information criterion
+#'  and returns which node is least important
+#'
+#' @param X Matrix of covariates
+#' @param y Vector of response
+#' @param q Number of hidden nodes
+#' @param n_init Number of random initialisations (tracks)
+#' @param inf_crit Information criterion: `"BIC"` (default), `"AIC"` or
+#'  `"AICc"`
+#' @param task `"regression"` (default) or `"classification"`
+#' @param unif Random initial values max value
+#' @param addition Switch for addition step (default FALSE)
+#' @param X_full Full matrix of covariates if X has some dropped
+#' @param maxit maximum number of iterations for nnet (default = 100)
+#' @param ... additional argument for nnet
+#' @return The least important input node
+#' @export
+input_importance <- function(X, y, q, n_init, inf_crit = "BIC",
+                             task = "regression", unif = 3,
+                             addition = FALSE, X_full = NULL, maxit = 1000, ...) {
+  if (addition == TRUE & is.null(X_full)) {
+    stop("Error: X_full must not be null to allow addition step")
+  }
+
+  if (addition == TRUE & (is.null(colnames(X_full)) | is.null(colnames(X)))) {
+    stop("Error: Column names must be supplied for X and X_full")
+  }
+
+  p_init <- ncol(X)
+
+  inf_crit_vec <- rep(NA, p_init)
+
+  weights_min <- vector("list", length = p_init)
+
+  for (p in 1:p_init) {
+    X_new <- X[, -p, drop = FALSE]
+
+    nn <- nn_fit_tracks(X_new, y, q, n_init, inf_crit, task, unif,
+                        maxit = maxit, ...
+    )
+
+    weights_min[[p]] <- nn$W_opt
+
+    inf_crit_vec[p] <- nn$value
+
+    names(inf_crit_vec)[p] <- colnames(X)[p]
+  }
+
+  if (addition == TRUE) {
+    dropped <- colnames(X_full)[!colnames(X_full) %in% colnames(X)]
+
+    i <- 1
+    for (p in dropped) {
+      X_new <- cbind(X, X_full[, p])
+
+      nn <- nn_fit_tracks(X_new, y, q, n_init, inf_crit, task, unif,
+                          maxit = maxit, ...
+      )
+
+      weights_min[[p_init + i]] <- nn$W_opt
+
+      inf_crit_vec[p_init + i] <- nn$value
+
+      names(inf_crit_vec)[p_init + i] <- p
+
+      i <- i + 1
+    }
+  }
+
+  W_opt <- weights_min[[which.min(inf_crit_vec)]]
+
+  return(list(
+    "min" = which.min(inf_crit_vec),
+    "value" = min(inf_crit_vec),
+    "inf_crit_vec" = inf_crit_vec,
+    "weights_min" = weights_min,
+    "W_opt" = W_opt
+  ))
+}
---
+++ R/selectnn.R
@@ -0,0 +1,346 @@
+
+#' Neural network model selection
+#'
+#' Performs both input and hidden layer selection for neural networks.
+#'
+#' @return A list with information of the optimal model.
+#' \itemize{
+#'   \item \code{W_opt} - vector of selected weights.
+#'   \item \code{value} - value of `"inf_crit"` for selected model.
+#'   \item \code{nn_hidden} - list of hidden node selection results.
+#'   \item \code{nn_input} - list of input node selection results.
+#'   \item \code{n_rep_h} - number of hidden node selection steps.
+#'   \item \code{n_rep_i} - number of input node selection steps.
+#'   \item \code{X} - matrix of the important covariates found.
+#'   \item \code{X_full} - matrix of all covariates.
+#'   \item \code{dropped} - vector of unimportant covariates.
+#'   \item \code{hidden_size} - vector of hidden layer size found at each step.
+#'   }
+#'
+#' @export
+selectnn <- function(...) UseMethod("selectnn")
+
+#' @rdname selectnn
+#' @param X Matrix of covariates
+#' @param y Vector of response
+#' @param Q Candidate number of hidden nodes
+#' @param n_init Number of random initialisations (tracks)`
+#' @param inf_crit Information criterion: `"BIC"` (default), `"AIC"` or
+#'  `"AICc"`
+#' @param task `"regression"` (default) or `"classification"`
+#' @param unif Random initial values max value
+#' @param maxit maximum number of iterations for nnet (default = 100)
+#' @param ... arguments passed to or from other methods
+#' @export
+selectnn.default <- function(X, y, Q, n_init, inf_crit = "BIC",
+                             task = "regression", unif = 3, maxit = 1000,
+                             ...) {
+  if (any(is.na(X)) | any(is.na(y))) {
+    stop("Error: Make sure data does not contain any NAs.")
+  }
+
+  if (!(inf_crit %in% c("BIC", "AIC", "AICc"))) {
+    stop(sprintf(
+      "Error: %s not recognised as information criterion.
+      Please choose from AIC, AICc or BIC.", inf_crit
+    ))
+  }
+
+  if (!(task %in% c("regression", "classification"))) {
+    stop(sprintf(
+      "Error: %s not recognised as task. Please choose from regression or classification.",
+      task
+    ))
+  }
+
+  cl <- match.call()
+
+
+  if (is.null(colnames(X))) {
+    colnames(X) <- colnames(X, do.NULL = FALSE, prefix = deparse(substitute(X)))
+  }
+
+  continue <- 1
+
+  hidden_size <- c()
+
+  n_rep_h <- 1
+
+  n_rep_i <- 1
+
+  nn_hidden <- vector(mode = "list", length = 1)
+
+  nn_input <- vector(mode = "list", length = 1)
+
+  nn_hidden[[1]] <- hidden_node_sel(
+    X = X,
+    y = y,
+    Q = Q,
+    n_init = n_init,
+    type = "bulk",
+    inf_crit = inf_crit,
+    task = task,
+    unif = unif,
+    maxit = maxit,
+    ...
+  )
+
+  hidden_size[1] <- nn_hidden[[1]]$min
+
+  nn_input[[1]] <- input_node_sel(
+    X = X,
+    y = y,
+    q = nn_hidden[[1]]$min,
+    n_init = n_init,
+    type = "bulk",
+    inf_crit = inf_crit,
+    task = task,
+    unif = unif,
+    maxit = maxit,
+    ...
+  )
+
+  if (!is.null(nn_input[[1]]$dropped)) {
+    X_new <- X[, !colnames(X) %in% nn_input[[1]]$dropped, drop = FALSE]
+    dropped <- nn_input[[1]]$dropped
+  } else {
+    X_new <- X
+    dropped <- c()
+  }
+
+
+  while (continue == 1) {
+    nn_hidden[[n_rep_h + 1]] <- hidden_node_sel(
+      X = X_new,
+      y = y,
+      Q = nn_hidden[[n_rep_h]]$min,
+      n_init = n_init,
+      type = "step",
+      inf_crit = inf_crit,
+      task = task,
+      unif = unif,
+      maxit = maxit,
+      ...
+    )
+
+    n_rep_h <- n_rep_h + 1
+
+    hidden_size[n_rep_h] <- nn_hidden[[n_rep_h]]$min
+
+    if (hidden_size[n_rep_h] == hidden_size[n_rep_h - 1]) {
+      continue <- 0
+    } else {
+      nn_input[[n_rep_i + 1]] <- input_node_sel(
+        X = X_new,
+        y = y,
+        q = nn_hidden[[n_rep_h]]$min,
+        n_init = n_init,
+        type = "step",
+        inf_crit = inf_crit,
+        task = task,
+        unif = unif,
+        X_full = X,
+        maxit = maxit,
+        ...
+      )
+
+      n_rep_i <- n_rep_i + 1
+
+      if (!is.null(nn_input[[n_rep_i]]$dropped) & ncol(X_new) > 1) {
+        X_new <- nn_input[[n_rep_i]]$X
+        dropped <- colnames(X)[!colnames(X) %in% colnames(X_new)]
+      } else {
+        continue <- 0
+      }
+
+      if (is.null(ncol(X_new))) {
+        continue <- 0
+      }
+    }
+  }
+
+  delta_bic <- nn_input[[n_rep_i]]$delta_bic
+
+  if (n_rep_i == 1) {
+    nn_input_extra <- input_node_sel(
+      X = X_new,
+      y = y,
+      q = nn_hidden[[n_rep_h]]$min,
+      n_init = n_init,
+      type = "step",
+      inf_crit = inf_crit,
+      task = task,
+      unif = unif,
+      X_full = X,
+      maxit = maxit,
+      ...
+    )
+
+    delta_bic <- nn_input_extra$delta_bic
+  }
+
+  p <- ncol(X_new)
+  q <- hidden_size[n_rep_h]
+  p_init <- ncol(X)
+  q_init <- Q
+
+  if (n_rep_h > n_rep_i) {
+    W_opt <- nn_hidden[[n_rep_h]]$W_opt
+    value <- nn_hidden[[n_rep_h]]$value
+  } else {
+    W_opt <- nn_input[[n_rep_i]]$W_opt
+    value <- nn_input[[n_rep_i]]$value
+  }
+
+
+  net <- list(
+    "W_opt" = W_opt,
+    "value" = value,
+    "nn_hidden" = nn_hidden,
+    "nn_input" = nn_input,
+    "n_rep_h" = n_rep_h,
+    "n_rep_i" = n_rep_i,
+    "X" = X_new,
+    "X_full" = X,
+    "dropped" = dropped,
+    "hidden_size" = hidden_size,
+    "p" = p,
+    "p_init" = p_init,
+    "q" = q,
+    "q_init" = q_init,
+    "delta_bic" = delta_bic,
+    "call" = cl
+  )
+
+  class(net) <- "selectnn"
+
+
+  return(net)
+}
+
+#' @rdname selectnn
+#' @param formula A formula of the form: response ~ x1 + x2 + ...
+#' @param data Data frame from which variables specified in formula are to be
+#'   taken
+#' @export
+selectnn.formula <- function(formula, data, ...) {
+  cl <- match.call()
+
+  mf <- stats::model.frame(formula, data = data)
+
+  y <- as.numeric(stats::model.response(mf))
+  X <- stats::model.matrix(formula, data = data)[, -1]
+
+  rownames(X) <- NULL
+
+  nn <- selectnn.default(X, y, ...)
+
+  nn$call <- cl
+
+  return(nn)
+}
+
+
+#' @export
+print.selectnn <- function(x, ...) {
+  cat("Call:\n")
+  print(x$call)
+  cat("\n")
+  cat("Model Selected: ", x$p, "-", x$q, "-", "1", " network",
+      sep = ""
+  )
+  cat(" with", (x$p + 2) * x$q + 1, "weights\n")
+  cat("Initial Model: ", x$p_init, "-", x$q_init, "-", "1", " network",
+      sep = ""
+  )
+  cat(" with", (x$p_init + 2) * x$q_init + 1, "weights\n")
+}
+
+#' @export
+coef.selectnn <- function(object, ...) {
+  wts <- object$W_opt
+  p <- object$p
+  q <- object$q
+
+  wm <- c(
+    "b",
+    paste(colnames(object$X), sep = ""),
+    paste("h", seq_len(q), sep = ""),
+    as.character(object$call$y)
+  )
+
+  conn <- c(rep(0:p, times = q), 0, (p + 1):(p + q))
+
+  nunits <- p + q + 2
+
+  nconn <- c(
+    rep(0, times = p + 2),
+    seq(p + 1, q * (p + 1), by = (p + 1)),
+    q * (p + 2) + 1
+  )
+
+  names(wts) <- apply(
+    cbind(
+      wm[1 + conn],
+      wm[1 + rep(1:nunits - 1, diff(nconn))]
+    ),
+    1,
+    function(x) paste(x, collapse = "->")
+  )
+  return(wts)
+}
+
+#' @export
+summary.selectnn <- function(object, ...) {
+  p <- object$p
+  q <- object$q
+
+  nconn <- c(
+    rep(0, times = p + 2),
+    seq(p + 1, q * (p + 1), by = (p + 1)),
+    q * (p + 2) + 1
+  )
+
+  object$nconn <- nconn
+
+  selected <-  !colnames(object$X_full) %in% object$dropped
+  covariates <- c(colnames(object$X_full)[selected],
+                  colnames(object$X_full)[!selected])
+  bic_diff <- object$delta_bic
+  selected_yesno <- c(rep("Yes", sum(selected)), rep("No", sum(!selected)))
+
+  coefdf <- data.frame(
+    Covariate = covariates,
+    Selected = selected_yesno,
+    `Delta BIC` = round(bic_diff, 3)
+  )
+
+  object$coefdf <- coefdf
+
+  class(object) <- c("summary.selectnn", class(object))
+  return(object)
+}
+
+
+#' @export
+print.summary.selectnn <- function(x, ...) {
+  cat("Call:\n")
+  print(x$call)
+  cat("\n")
+  cat("Number of input nodes:", x$p, "\n")
+  cat("Number of hidden nodes:", x$q, "\n")
+  cat("\n")
+  cat("Value:", x$val, "\n")
+  cat("\n")
+  cat("Inputs:\n")
+  print(x$coefdf,
+        row.names = FALSE
+  )
+  cat("\n")
+  cat("Weights:\n")
+  wts <- format(round(coef.selectnn(x), 2))
+  lapply(
+    split(wts, rep(1:(x$p + x$q + 2), diff(x$nconn))),
+    function(x) print(x, quote = FALSE)
+  )
+}
